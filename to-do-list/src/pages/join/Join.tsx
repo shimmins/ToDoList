27f196ff86d5a41af6f42bba4067ae96cebe85a7
yujin <uj0791navercom@skuniv.ac.kr>
2023-03-13 09:19:13 +0900

    .

--- to-do-list/src/pages/join/Join.tsx
+++ to-do-list/src/pages/join/Join.tsx
@@ -1,70 +1,122 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button } from 'antd';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "antd";
 
-// interface userInformation {
-//     name : '';
-//     id: '';
-//     pw: '';
-// }
+export function Join() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
 
-export function Join(){
-    const[name, setName] = useState('');
-    const[id, setId] = useState('');
-    const[password, setPassword] = useState('');
-    const onName = (event: React.ChangeEvent<HTMLInputElement>) =>{
-        setName(event.target.value);
-    }
-    const onId = (event: React.ChangeEvent<HTMLInputElement>) =>{
-        setId(event.target.value);
-    }
-    const onPassword = (event: React.ChangeEvent<HTMLInputElement>) =>{
-        setPassword(event.target.value);
-    }
-    
-    const navigate = useNavigate();
+  const onId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setId(event.target.value);
+  };
+  const onPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
-    const onSubmit = () =>{
-        localStorage.setItem(id, password);
-        navigate('../login');
-    }
+  const navigate = useNavigate();
 
-    return(
-        <div style={{display: 'flex', flexDirection: 'column',alignItems: 'center', backgroundColor: '	#E6E6FA', height: '100vh', width: '100%'}}>
-            <div>
-                <h1>To-Do List</h1>
-            </div>
-            <div>
-                <div>이름</div>
-                <input style={{height: '20px', width: '100%'}}
-                value={name}
-                onChange={onName}
-                type="text"
-                />
-            </div>
-            <div>
-                <div>아이디</div>
-                <input style={{height: '20px', width: '100%'}}
-                value={id}
-                onChange={onId}
-                type="text"
-                />
-            </div>
-            <div>
-                <div>비밀번호</div>
-                <input style={{height: '20px', width: '100%'}}
-                value={password}
-                onChange={onPassword}
-                type="text"
-                />
-            </div>
-            <div style={{width: '100px'}}>
-                <br></br>
-                <Button onClick={onSubmit} type="primary" block>
-                    회원가입
-                </Button>
-            </div>
+  const onSubmit = () => {
+    localStorage.setItem(id, password);
+    navigate("../login");
+  };
 
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "	#E6E6FA",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          width: "40%",
+          margin: "90px",
+          borderRadius: "10px",
+          backgroundColor: "white",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            color: "#8A2BE2",
+          }}
+        >
+          <h1>To-Do List</h1>
         </div>
-    );
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            margin: "20px",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              width: "55%",
+              color: "gray",
+            }}
+          >
+            UserID
+          </div>
+          <input
+            style={{
+              backgroundColor: "#E6E6FA",
+              height: "30px",
+              width: "60%",
+              border: "none",
+              borderRadius: "10px",
+              margin: "5px",
+            }}
+            value={id}
+            onChange={onId}
+            type="text"
+          />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              width: "55%",
+              color: "gray",
+            }}
+          >
+            UserPassword
+          </div>
+          <input
+            style={{
+              backgroundColor: "#E6E6FA",
+              height: "30px",
+              width: "60%",
+              border: "none",
+              borderRadius: "10px",
+              margin: "5px",
+            }}
+            value={password}
+            onChange={onPassword}
+            type="text"
+          />
+          <div style={{ width: "100px" }}>
+            <br></br>
+            <Button
+              style={{ backgroundColor: "#8A2BE2" }}
+              size={"large"}
+              onClick={onSubmit}
+              type="primary"
+              block
+            >
+              Join
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
