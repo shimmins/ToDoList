1a11effecbe0d5bd42e4c1c000bede515292ba02
yujin <uj0791navercom@skuniv.ac.kr>
2024-01-08 17:48:44 +0900

    feat: get
    "/" 접속시 hello 출력
    post 수정 필요

--- db-server/index.js
+++ db-server/index.js
@@ -1,37 +1,54 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 const mysql = require("mysql");
 const PORT = process.env.port || 8000;
 
 let corsOptions = {
-  origin: "*", // 출처 허용 옵션
-  credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+  origin: "*",
+  credentials: true, // 사용자 인증이 필요한 리소스 접근
 };
-
 app.use(cors(corsOptions));
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "0000abc",
-  database: "bbs",
-});
-
-app.get("/mypage", (req, res) => {
-  const sqlQuery = "INSERT INTO requested (rowno) VALUES (1)";
-  db.query(sqlQuery, (err, result) => {
-    console.log(err);
-    res.send("success!");
-  });
+//sql 연동
+var db = mysql.createPool({
+  host: "db-server.cabsry9iojbu.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "soldesk1.",
+  database: "db_server",
 });
 
 app.get("/", (req, res) => {
-  const sqlQuery = "SELECT * FROM TODOLIST;";
+  res.send('hello');
+});
+
+app.get("/home", (req, res) => {
+  const sqlQuery = "SELECT * FROM db.TODOLIST;";
   db.query(sqlQuery, (err, result) => {
     res.send(result);
   });
 });
+
+//todo
+app.post("/home", (req, res) => {
+  const todo = req.body;
+  console.log(todo);
+  const sqlQuery =
+    "INSERT INTO TODOLIST(TODO_CONTENT, TODO_CHECK) VALUES (?, 0);";
+
+  db.query(sqlQuery, [todo.content], (err, result) => {
+    if (err) console.log(err);
+    else res.send(result);
+  });
+});
+
+// app.get("/mypage", (req, res) => {
+//   const sqlQuery = "INSERT INTO requested (rowno) VALUES (1)";
+//   db.query(sqlQuery, (err, result) => {
+//     console.log(err);
+//     res.send("success!");
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}`);
